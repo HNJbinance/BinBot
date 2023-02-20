d54595e541df629fc7005d5d23da04624226d02d
souhila <leb_sou92@yahoo.fr>
2023-02-20 14:42:50 +0100

    ajout plus de paramatres d'entrées

--- scripts/import websocket.py
+++ scripts/import websocket.py
@@ -7,11 +7,23 @@ def on_message(ws, message):
    if data['e'] == 'error':
      print(f"Error: {data['m']}")
    else:
-        btc_real_price = float(data['c'])
-        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        symbol = data['s']   # Trading symbol
+        price = float(data['c'])   # Last price
+        bid_price = float(data['b'])   # Current best bid price
+        ask_price = float(data['a'])   # Current best ask price
+        trade_volume = float(data['v'])   # 24h trading volume
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')   # Current timestamp
         with open('btc_real_price.json', 'a') as f:
-            json.dump({'timestamp': timestamp, 'BTC/USDT': btc_real_price}, f)
+            json.dump({
+                'timestamp': timestamp,
+                'symbol': symbol,
+                'last_price': price,
+                'best_bid_price': bid_price,
+                'best_ask_price': ask_price,
+                '24h_volume': trade_volume
+            }, f)
             f.write('\n')
+
 
 def on_error(ws, error):
     print(f"WebSocket error: {error}")
