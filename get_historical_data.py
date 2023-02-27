1c96a5c4934f607ddf9986ca80026a666b4acb58
Loic <loic.montagnac@gmail.com>
2023-02-27 19:33:19 +0000

    Update get_historical_data.py & sql_properties.py to update startime colums of symbol_interval table

--- get_historical_data.py
+++ get_historical_data.py
@@ -11,50 +11,60 @@ client = Spot()
 sql = sql.SqlAction()
 
 # print(client.time())
-db_next_startime = 0
-starttime = 1
-i=0
-max_loop =1000
-while ( db_next_startime < starttime and i < max_loop ) : 
+
+# get symbol interval data to retrieve historical data from database
+symint = sql.get_symbol_interval()
+# loop on every symbol/interval requested
+for val in symint :    
+    # variables   
+    api_starttime = 0
+    api_endtime = 1
+    i = 0
+    max_loop = 1000 
+    # db variables   
+    db_id_symint = val['id_symint']
+    db_starttime = val['starttime']
+    db_endtime = val['endtime']
+    db_interval=val['interval_symbol']
+    db_symbol = val['symbol']
     
-    # get symbol interval data to retrieve historical data from database
-    symint = sql.get_symbol_interval()
-    # loop on every symbol/interval requested
-    for val in symint :
-        # db variables   
-        db_id_symint = val['id_symint']
-        db_next_startime = val['endtime']+1
-        db_interval=val['interval_symbol']
-        db_symbol = val['symbol']
-        
+    api_starttime == db_endtime
+    print(colored('get_historical_date : db_id_symint:{}, db_symbol:{}, db_interval:{}, db_starttime:{}, db_endtime:{}'.format(db_id_symint, val['symbol'], val['interval_symbol'], val['starttime'], val['endtime']),"green"))
+
+    while ( api_starttime < api_endtime and i < max_loop ) : 
         # get historical data of symbol/interval requested from binance REST API
-        klines_data =  client.klines(symbol=db_symbol, interval=db_interval,startTime=db_next_startime)
+        klines_data =  client.klines(symbol=db_symbol, interval=db_interval,startTime=db_endtime+1)
         # Remove last index of each list (// Unused field. Ignore.)
         klines_data = [klines_data[:-1] for klines_data in klines_data]
 
         # api variables  
         lenght_list = len(klines_data)-1    
-        starttime = klines_data[0][0] if lenght_list>0 else 0
-        endtime = klines_data[lenght_list][0] if lenght_list>0 else 0
-       
-        print(colored('get_historical_date : db_id_symint:{}, db_symbol:{}, db_interval:{}, db_starttime:{}, db_endtime:{}'.format(db_id_symint, val['symbol'], val['interval_symbol'], val['starttime'], val['endtime']),"green"))
-        print(colored('get_historical_date : api_next_starttime:{}, api_next_endtime:{}'.format(starttime,endtime),"yellow"))
+        api_starttime = klines_data[0][0] if lenght_list>0 else 0
+        api_endtime = klines_data[lenght_list][0] if lenght_list>0 else 0
+    
+        print(colored('get_historical_date : api_starttime:{}, api_endtime:{}'.format(api_starttime,api_endtime),"yellow"))
 
         if lenght_list > 0 :
             # Storing API result into database
             sql.store_historical_klines(db_id_symint,klines_data)
 
             # Update last stoptime into database
-            sql.update_symbol_interval(db_id_symint,endtime)
+            sql.update_symbol_interval_endtime(db_id_symint,api_endtime)
+            db_endtime = api_endtime
+
+            if db_starttime == 0 : 
+                # Update first starttime into database
+                sql.update_symbol_interval_starttime(db_id_symint,api_starttime)
+                db_starttime = api_starttime
 
             # Waiting for API limitation
             sleep(1)
             i += 1
-else :
-    if db_next_startime > starttime : 
-         print(colored("get_historical_date : historical data is up to date" ,'green'))
-    elif i > max_loop :
-        print(colored("get_historical_date : max_loop is reached please launch again" ,'green'))
+    else :
+        if db_endtime > api_starttime : 
+            print(colored("get_historical_date : historical data is up to date" ,'green'))
+        elif i > max_loop :
+            print(colored("get_historical_date : max_loop is reached please launch again" ,'green'))
 
 
 
