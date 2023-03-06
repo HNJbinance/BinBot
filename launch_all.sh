fad454837bb2290e3d3d02220fe5f89ac35b9ea8
Loic <loic.montagnac@gmail.com>
2023-03-06 17:36:22 +0000

    correct path

--- launch_all.sh
+++ launch_all.sh
@@ -4,10 +4,10 @@ sleep 2
 
 echo "starting get_stream_data.py in background..."
 pkill -f get_stream_data.py
-python3 /home/ubuntu/datascientest/000-project/OPA/BinBot/get_stream_data.py </dev/null &>/dev/null &
+python3 get_stream_data.py </dev/null &>/dev/null &
 
 echo "starting get_historical_data.py in background..."
 pkill -f get_historical_data.py
-python3 /home/ubuntu/datascientest/000-project/OPA/BinBot/get_historical_data.py  </dev/null &>/dev/null &
+python3 get_historical_data.py  </dev/null &>/dev/null &
 
 
