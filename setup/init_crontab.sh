473908fc5e1fb749e265e513a9324b1ba9167ac9
LoicM <loic.montagnac@gmail.com>
2023-03-14 19:02:58 +0000

    .

--- setup/init_crontab.sh
+++ setup/init_crontab.sh
@@ -4,9 +4,9 @@ echo Schedule get_historical_data.py every hours...
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "0 * * * * python3 $BinBotDir/../purge_stream_data.py" >> mycron
-echo "5 * * * * python3 $BinBotDir/../get_stream_data.py" >> mycron
-echo "10 * * * * python3 $BinBotDir/../get_historical_data.py" >> mycron
+echo "0 * * * * python3 $BinBotDir/purge_stream_data.py" >> mycron
+echo "5 * * * * python3 $BinBotDir/get_stream_data.py" >> mycron
+echo "10 * * * * python3 $BinBotDir/get_historical_data.py" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
