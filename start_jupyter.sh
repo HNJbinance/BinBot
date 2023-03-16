42a955f77936626226357adeaeb918c4c27f6d6c
Ilham Noumir <58338348+IlhamNOUMIR@users.noreply.github.com>
2023-03-16 22:04:01 +0100

    Delete start_jupyter.sh

--- start_jupyter.sh
+++
@@ -1,6 +0,0 @@
-echo starting jupyter notebook...
-pkill -f jupyter-notebook
-cd .. 
-jupyter notebook --no-browser --ip="ec2-54-78-128-185.eu-west-1.compute.amazonaws.com" </dev/null &>/dev/null &
-
- 
