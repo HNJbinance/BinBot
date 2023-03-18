b359c1b920f8308802828c0703d939605f5d4da2
Ubuntu <ubuntu@ip-172-31-33-175.eu-west-1.compute.internal>
2023-03-18 18:02:25 +0000

    add get(/model_performance) endpoint

--- api/main.py
+++ api/main.py
@@ -5,6 +5,8 @@ from datetime import timedelta, datetime
 import pandas as pd
 from fastapi import FastAPI
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
 import pickle
 import warnings
 warnings.filterwarnings('ignore')
@@ -28,23 +30,6 @@ cursor = cnx.cursor()
 # df = pd.read_csv("../model/data.csv")
 model = pickle.load(open('../model/rf_regressor.pkl','rb'))
 ##############################################################################################################
-#                                Loading data from opa database 
-##############################################################################################################
-query1 = "SELECT * FROM historical_klines"
-cursor.execute(query1)
-
-# Load the data into a Pandas DataFrame
-data = pd.DataFrame(cursor.fetchall(), columns=['id_symint', 'open_time', 'open_price', 'high_price', 'low_price',
-       'close_price', 'volume', 'close_time', 'quote_asset_volume',
-       'number_of_trades', 'taker_buy_base_asset_volume',
-       'taker_buy_quote_asset_volume'])
-
-query2 = "SELECT close_price FROM stream_klines"
-cursor.execute(query2)
-result = cursor.fetchall()
-close_price_stream_list = [row[0] for row in result]
-close_price_stream = close_price_stream_list[0]
-##############################################################################################################
 #                                Constantes
 ##############################################################################################################
 # Authentifiaction & authorisation :
@@ -60,6 +45,41 @@ lag_columns = ['open_price', 'high_price', 'low_price', 'close_price', 'volume',
                'taker_buy_quote_asset_volume']
 
 lag_count = 6
+##############################################################################################################
+#                                prepare data for prediction and performance test
+##############################################################################################################
+query1 = "SELECT * FROM historical_klines"
+cursor.execute(query1)
+# Load the data into a Pandas DataFrame
+data = pd.DataFrame(cursor.fetchall(), columns=['id_symint', 'open_time', 'open_price', 'high_price', 'low_price',
+       'close_price', 'volume', 'close_time', 'quote_asset_volume',
+       'number_of_trades', 'taker_buy_base_asset_volume',
+       'taker_buy_quote_asset_volume'])
+
+
+data['close_time'] = pd.to_datetime(data['close_time'], unit='ms')
+data = data.set_index('close_time')
+data.sort_index(inplace=True)
+
+# Fill any missing values
+data = data.ffill()    
+# Create lagged features for the Random Forest model
+for col in lag_columns:
+    for lag in range(1, lag_count + 1):
+        data[f'{col}_lag{lag}'] = data[col].shift(lag)
+
+# Supprimer les lignes avec des valeurs manquantes après la création des colonnes décalées
+data.dropna(inplace=True)
+
+# Split the data into training and testing sets
+feats = data.drop(['close_price', 'id_symint', 'open_time'], axis=1)
+target = data['close_price']
+
+query2 = "SELECT close_price FROM stream_klines"
+cursor.execute(query2)
+result = cursor.fetchall()
+close_price_stream_list = [row[0] for row in result]
+close_price_stream = close_price_stream_list[0]
 ##############################################################################################################
 #                                functions 
 ##############################################################################################################
@@ -80,31 +100,47 @@ def decision(actual_close_price: float, predict_close_price: float):
         return "sell"
     else:
         return "hold"
+
+def add_user_to_db(user):
+    pass 
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+def calculate_model_performance():
+    global feats
+    # Chargez le modèle à partir du fichier .pkl
+    model = pickle.load(open('../model/rf_regressor.pkl', 'rb'))
+    
+    # Utilisez les données de test ou de validation pour obtenir les prédictions
+
+    # Vous pouvez utiliser les données de test que vous avez créées précédemment
+    y_pred = model.predict(feats)
+    
+    # Calculez les métriques de performance
+    mae = mean_absolute_error(target, y_pred)
+    mse = mean_squared_error(target, y_pred)
+    rmse = mean_squared_error(target, y_pred, squared=False)
+    r2 = r2_score(target, y_pred)
+    
+    performance_metrics = {
+        "mean_absolute_error": mae,
+        "mean_squared_error": mse,
+        "root_mean_squared_error": rmse,
+        "r2_score": r2
+    }
+    
+    return performance_metrics
+
 ##############################################################################################################
 #                                api 
 ##############################################################################################################
 
 @api.get("/predict")
 def predict_close_price(username: str = Depends(get_current_username)):
+    # global data
+    global feats
     global data
     # data = data.copy()
-    data['close_time'] = pd.to_datetime(data['close_time'], unit='ms')
-    data = data.set_index('close_time')
-    data.sort_index(inplace=True)
-
-    # Fill any missing values
-    data = data.ffill()    
-    # Create lagged features for the Random Forest model
-    for col in lag_columns:
-        for lag in range(1, lag_count + 1):
-            data[f'{col}_lag{lag}'] = data[col].shift(lag)
-
-    # Supprimer les lignes avec des valeurs manquantes après la création des colonnes décalées
-    data.dropna(inplace=True)
-
-    # Split the data into training and testing sets
-    feats = data.drop(['close_price', 'id_symint', 'open_time'], axis=1)
-    target = data['close_price']
+    
 
     # Predict the close price for the next hour
 
@@ -122,4 +158,25 @@ def predict_close_price(username: str = Depends(get_current_username)):
             "decision":decision(close_price_stream, next_hour_close_price)}
 
 
+
+@api.get("/price_history/{symbol}/{interval}")
+def get_price_history(symbol: str, interval: str, start_time: str, end_time: str, username: str = Depends(get_current_username)):
+    # Ici, vous pouvez écrire le code pour récupérer l'historique des prix pour le symbole et l'intervalle donnés
+    # Par exemple, interroger la base de données pour obtenir l'historique des prix
+    price_history = get_price_history_from_db(symbol, interval, start_time, end_time) # Cette fonction doit être définie par vous
+
+    return {"symbol": symbol, "interval": interval, "price_history": price_history}
+
+@api.post("/add_user")
+def add_user(user: dict, username: str = Depends(get_current_username)):
+    # Ici, vous pouvez écrire le code pour ajouter un nouvel utilisateur à la base de données
+    # Par exemple, insérer les informations de l'utilisateur dans la table des utilisateurs
+    user_id = add_user_to_db(user) # Cette fonction doit être définie par vous
+
+    return {"message": "User added successfully", "user_id": user_id}
+
+@api.get("/model_performance")
+def get_model_performance(username: str = Depends(get_current_username)):
+    performance_metrics = calculate_model_performance() 
+    return {"model_performance": performance_metrics}
 
