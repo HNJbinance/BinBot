fce15a6789d55086078f573f2e6807bf109983b3
Ubuntu <ubuntu@ip-172-31-33-175.eu-west-1.compute.internal>
2023-03-19 00:19:58 +0000

    add get price history endpoint

--- api/main.py
+++ api/main.py
@@ -121,6 +121,20 @@ def calculate_model_performance():
         "r2_score": r2
     }
     return performance_metrics
+    
+def get_price_history_from_db(start_time: str, end_time: str, interval: str, symbole:str ='BTC'):
+    global data
+    df_price= data[['close_price']]
+    df_price.index = pd.to_datetime(df_price.index)
+    filtered_data = df_price[start_time:end_time]
+    resampled_data = filtered_data.resample(interval).last()
+    price_history = resampled_data.to_dict()
+    prices_data = []
+    for date, price in price_history['close_price'].items():
+        prices_data.append({"symbol":"BTC","interval":interval,"date": date.strftime('%Y-%m-%d %H:%M:%S'), "price": price})
+    
+    return prices_data
+
 
 ##############################################################################################################
 #                                api 
@@ -144,12 +158,23 @@ def predict_close_price(username: str = Depends(get_current_username)):
             "predicted_close_price": round(next_hour_close_price, 2),  
             "decision":decision(close_price_stream, next_hour_close_price)}
 
-@api.get("/price_history/{symbol}/{interval}")
-def get_price_history(symbol: str, interval: str, start_time: str, end_time: str, username: str = Depends(get_current_username)):
-    price_history = get_price_history_from_db(symbol, interval, start_time, end_time) 
-    return {"symbol": symbol, 
-            "interval": interval, 
-            "price_history": price_history}
+@api.get("/price_history/{start_time}/{end_time}/{interval}/{symbol}")
+def get_price_history(start_time: str, end_time: str, interval: str, symbol:str ='BTC', username: str = Depends(get_current_username)):
+    """
+    Récupère l'historique des prix d'une crypto-monnaie entre deux dates spécifiées, avec une fréquence définie.
+    Args:
+        start_time (str): Date de début sous forme de chaîne de caractères au format 'YYYY-MM-DD'.
+        end_time (str): Date de fin sous forme de chaîne de caractères au format 'YYYY-MM-DD'.
+        interval (str): Fréquence de l'échantillonnage. '1H' pour une heure, '7D' pour une semaine, '1M' pour un mois .
+        symbole (str): Symbole de la crypto-monnaie (par défaut 'BTC').
+    
+    Exemple:
+        Pour récupérer l'historique des prix du Bitcoin (BTC) entre  :  '2023-02-17 16:59:59' ET '2023-03-17 20:59:59'
+        
+        >>> Sur Postamn :http://127.0.0.1:8000/price_history/2023-02-16 16:59:59/2023-03-16 20:59:59/7D/BTC
+    """
+    price_history = get_price_history_from_db(start_time, end_time, interval, symbol)
+    return price_history
 
 @api.post("/add_user")
 def add_user(user: dict, username: str = Depends(get_current_username)):
