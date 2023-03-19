c9cae4a6ae856c1c2281cd8f8584ae72988e316d
Ubuntu <ubuntu@ip-172-31-33-175.eu-west-1.compute.internal>
2023-03-19 21:44:28 +0000

    update test api

--- api/test_main.py
+++ api/test_main.py
@@ -5,7 +5,7 @@ from main import api, get_price_history_from_db
 client = TestClient(api)
 
 def test_predict_close_price():
-    response = client.get("/predict", auth=("ilham", "noumir"))
+    response = client.get("/predict", auth=("hamza", "ennaji"))
     assert response.status_code == 200
     assert "predicted_close_price" in response.json()
     assert "decision" in response.json()
@@ -13,19 +13,19 @@ def test_predict_close_price():
 def test_get_price_history():
     start_time = "2023-02-01 00:00:00"
     end_time = "2023-02-28 23:59:59"
-    interval = "7D"
-    response = client.get(f"/price_history/{start_time}/{end_time}/{interval}/BTC", auth=("ilham", "noumir"))
+    interval = "1H"
+    response = client.get(f"/price_history/{start_time}/{end_time}/{interval}/BTC", auth=("hamza", "ennaji"))
     assert response.status_code == 200
     assert len(response.json()) > 0
 
 def test_add_user():
     new_user = {"username": "test_user", "password": "test_password"}
-    response = client.post("/add_user", json=new_user, auth=("ilham", "noumir"))
+    response = client.post("/add_user", json=new_user, auth=("hamza", "ennaji"))
     assert response.status_code == 200
     assert "user_id" in response.json()
 
 def test_get_model_performance():
-    response = client.get("/model_performance", auth=("ilham", "noumir"))
+    response = client.get("/model_performance", auth=("hamza", "ennaji"))
     assert response.status_code == 200
     assert "model_performance" in response.json()
     assert "mean_absolute_error" in response.json()["model_performance"]
