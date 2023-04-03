434dd3bd793c334d38c916d64ca223f1162eed3b
Ubuntu <ubuntu@ip-172-31-33-175.eu-west-1.compute.internal>
2023-04-03 14:41:11 +0000

    commit last modifications

--- start.sh
+++ start.sh
@@ -1,13 +1,17 @@
+echo "stoping recent docker"
+docker stop $(docker ps -aq) 
+
+echo "activating virtualenv"
+source ./binbotvenv/bin/activate 
+
 echo "starting mysql container..."
 docker start mysql_container
 sleep 2
 
-echo "starting get_stream_data.py in background..."
-pkill -f get_stream_data.py
-python3 get_stream_data.py </dev/null &>/dev/null &
-
 echo "starting get_historical_data.py in background..."
 pkill -f get_historical_data.py
-python3 get_historical_data.py  </dev/null &>/dev/null &
+python3 get_historical_data.py  
 
-
+echo "starting get_stream_data.py in background..."
+pkill -f get_stream_data.py
+python3 get_stream_data.py 
